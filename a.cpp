bf5e07b4146760a81c39e05a20844da898262579
Apurbo73 <apurbodebnath895@gmail.com>
2025-06-09 00:26:33 +0600

    done

---
+++ a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        vector<int> A(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> A[i];
+        }
+
+        int M = *min_element(A.begin(), A.end());
+        int operations = 0;
+        for (int i = 0; i < N; ++i) {
+            if (A[i] > M) {
+                operations++;
+            }
+        }
+
+        cout << operations << endl;
+    }
+    return 0;
+}
